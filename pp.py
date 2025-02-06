d7b09b2b1b06730e283f25780d5f822ca9eacef1
Aibar <a_jenissov@kbtu.kz>
2025-02-07 04:59:39 +0500

    Get_started

--- pp.py
+++ pp.py
@@ -1,24 +1,36 @@
 '''
-What is Python?
-Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+Python Install
+Many PCs and Macs will have python already installed.
 
-It is used for:
-    web development (server-side),
-    software development,
-    mathematics,
-    system scripting.
+To check if you have python installed on a Windows PC, search in the start bar for Python or run the following on the Command Line (cmd.exe):
 
-What can Python do?
-    Python can be used on a server to create web applications.
-    Python can be used alongside software to create workflows.
-    Python can connect to database systems. It can also read and modify files.
-    Python can be used to handle big data and perform complex mathematics.
-    Python can be used for rapid prototyping, or for production-ready software development.
+C:\Users\Your Name>python --version
+To check if you have python installed on a Linux or Mac, then on linux open the command line or on Mac open the Terminal and type:
 
-Why Python?
-    Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
-    Python has a simple syntax similar to the English language.
-    Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
-    Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
-    Python can be treated in a procedural way, an object-oriented way or a functional way.
+python --version
+If you find that you do not have Python installed on your computer, then you can download it for free from the following website: https://www.python.org/
+
+Python Quickstart
+Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+
+The way to run a python file is like this on the command line:
+
+C:\Users\Your Name>python helloworld.py
+Where "helloworld.py" is the name of your python file.
+
+Let's write our first Python file, called helloworld.py, which can be done in any text editor.
+
+Where "helloworld.py" is the name of your python file.
+
+Let's write our first Python file, called helloworld.py, which can be done in any text editor.
+'''
+print("Hello, World!")
+'''
+Simple as that. Save your file. Open your command line, navigate to the directory where you saved your file, and run:
+
+C:\Users\Your Name>python helloworld.py
+
+The output should read:
+
+Hello, World!
 '''
