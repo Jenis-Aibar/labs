1b8c2a4de9c711f1a674f50e11a66c6e7e4bf744
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:39:40 +0500

    Set_Methods

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,77 +1,27 @@
-# Python - Join Sets
-# Join Sets
-# There are several ways to join two or more sets in Python.
+# Python - Set Methods
+# Set Methods
+# Python has a set of built-in methods that you can use on sets.
 
-# The union() and update() methods joins all items from both sets.
 
-# The intersection() method keeps ONLY the duplicates.
-
-# The difference() method keeps the items from the first set that are not in the other set(s).
-
-# The symmetric_difference() method keeps all items EXCEPT the duplicates.
-
-# Union
-# The union() method returns a new set with all items from both sets.
-
-# Join set1 and set2 into a new set:
-
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-
-set3 = set1.union(set2)
-print(set3)
-# You can use the | operator instead of the union() method, and you will get the same result.
-
-# Use | to join two sets:
-
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-
-set3 = set1 | set2
-print(set3)
-# Join Multiple Sets
-# All the joining methods and operators can be used to join multiple sets.
-
-# When using a method, just add more sets in the parentheses, separated by commas:
-
-# Join multiple sets with the union() method:
-
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
-
-myset = set1.union(set2, set3, set4)
-print(myset)
-# When using the | operator, separate the sets with more | operators:
-# Use | to join two sets:
-
-set1 = {"a", "b", "c"}
-set2 = {1, 2, 3}
-set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
-
-myset = set1 | set2 | set3 |set4
-print(myset)
-# Join a Set and a Tuple
-# The union() method allows you to join a set with other data types, like lists or tuples.
-
-# The result will be a set.
-# Join a set with a tuple:
-
-x = {"a", "b", "c"}
-y = (1, 2, 3)
-
-z = x.union(y)
-print(z)
-# Note: The  | operator only allows you to join sets with sets, and not with other data types like you can with the  union() method.
-# Update
-# The update() method inserts all items from one set into another.
-# The update() changes the original set, and does not return a new set.
-# The update() method inserts the items in set2 into set1:
-
-set1 = {"a", "b" , "c"}
-set2 = {1, 2, 3}
-
-set1.update(set2)
-print(set1)
+'''
+Method	Shortcut	Description
+add()	 	Adds an element to the set
+clear()	 	Removes all the elements from the set
+copy()	 	Returns a copy of the set
+difference()	-	Returns a set containing the difference between two or more sets
+difference_update()	-=	Removes the items in this set that are also included in another, specified set
+discard()	 	Remove the specified item
+intersection()	&	Returns a set, that is the intersection of two other sets
+intersection_update()	&=	Removes the items in this set that are not present in other, specified set(s)
+isdisjoint()	 	Returns whether two sets have a intersection or not
+issubset()	<=	Returns whether another set contains this set or not
+ 	<	Returns whether all items in this set is present in other, specified set(s)
+issuperset()	>=	Returns whether this set contains another set or not
+ 	>	Returns whether all items in other, specified set(s) is present in this set
+pop()	 	Removes an element from the set
+remove()	 	Removes the specified element
+symmetric_difference()	^	Returns a set with the symmetric differences of two sets
+symmetric_difference_update()	^=	Inserts the symmetric differences from this set and another
+union()	|	Return a set containing the union of sets
+update()	|=	Update the set with the union of this set and others
+'''
