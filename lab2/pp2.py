edf8532ee54110ca16c8725392378f98b09aee1f
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:10:48 +0500

    Add_List_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,46 +1,48 @@
-# Python - Change List Items
+# Python - Add List Items
 
 
-# Change Item Value
-# To change the value of a specific item, refer to the index number:
-# Change the second item:
+# Append Items
+# To add an item to the end of the list, use the append() method:
 
-thislist = ["apple", "banana", "cherry"]
-thislist[1] = "blackcurrant"
-print(thislist)
-
-# Change a Range of Item Values
-# To change the value of items within a specific range, define a list with the new values, and refer to the range of index numbers where you want to insert the new values:
-# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
-thislist[1:3] = ["blackcurrant", "watermelon"]
-print(thislist)
-
-# If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
-
-# Change the second value by replacing it with two new values:
+# ExampleGet your own Python Server
+# Using the append() method to append an item:
 
 thislist = ["apple", "banana", "cherry"]
-thislist[1:2] = ["blackcurrant", "watermelon"]
-print(thislist)
-#-----------------------------------------------------------------------------------------------------------------------
-# Note: The length of the list will change when the number of items inserted does not match the number of items replaced.
-#-----------------------------------------------------------------------------------------------------------------------
-
-# If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
-# Change the second and third value by replacing it with one value:
-
-thislist = ["apple", "banana", "cherry"]
-thislist[1:3] = ["watermelon"]
+thislist.append("orange")
 print(thislist)
 
 # Insert Items
-# To insert a new list item, without replacing any of the existing values, we can use the insert() method.
+# To insert a list item at a specified index, use the insert() method.
+
 # The insert() method inserts an item at the specified index:
 
-# Insert "watermelon" as the third item:
+# Insert an item as the second position:
 
 thislist = ["apple", "banana", "cherry"]
-thislist.insert(2, "watermelon")
+thislist.insert(1, "orange")
+print(thislist)
+#------------------------------------------------------------------------------
+# Note: As a result of the examples above, the lists will now contain 4 items.
+#------------------------------------------------------------------------------
+
+# Extend List
+# To append elements from another list to the current list, use the extend() method.
+
+# Add the elements of tropical to thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+# The elements will be added to the end of the list.
+
+# Add Any Iterable
+# The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+
+# Add elements of a tuple to a list:
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
 print(thislist)
