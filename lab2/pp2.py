0d25ea2677f086932f1cabf80a201230d5708126
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:45:03 +0500

    Copy_Dictionaries

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,26 +1,25 @@
-# Python - Loop Dictionaries
-# Loop Through a Dictionary
-# You can loop through a dictionary by using a for loop.
+# Python - Copy Dictionaries
+# Copy a Dictionary
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
 
-# When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
 
-# Print all key names in the dictionary, one by one:
+# Make a copy of a dictionary with the copy() method:
 
-for x in thisdict:
-  print(x)
-# Print all values in the dictionary, one by one:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+# Another way to make a copy is to use the built-in function dict().
+# Make a copy of a dictionary with the dict() function:
 
-for x in thisdict:
-  print(thisdict[x])
-# You can also use the values() method to return values of a dictionary:
-
-for x in thisdict.values():
-  print(x)
-# You can use the keys() method to return the keys of a dictionary:
-
-for x in thisdict.keys():
-  print(x)
-# Loop through both keys and values, by using the items() method:
-
-for x, y in thisdict.items():
-  print(x, y)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
