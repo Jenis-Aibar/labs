3256c0079beb3cfc5ac6b818e86391f4ae5d5c9c
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:25:50 +0500

    Tuple_Methods

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,19 +1,7 @@
-# Python - Join Tuples
-# Join Two Tuples
-# To join two or more tuples you can use the + operator:
-# Join two tuples:
+# Python - Tuple Methods
+# Tuple Methods
+# Python has two built-in methods that you can use on tuples.
 
-tuple1 = ("a", "b" , "c")
-tuple2 = (1, 2, 3)
-
-tuple3 = tuple1 + tuple2
-print(tuple3)
-# Multiply Tuples
-# If you want to multiply the content of a tuple a given number of times, you can use the * operator:
-
-# Multiply the fruits tuple by 2:
-
-fruits = ("apple", "banana", "cherry")
-mytuple = fruits * 2
-
-print(mytuple)
+# Method	Description
+# count()	Returns the number of times a specified value occurs in a tuple
+# index()	Searches the tuple for a specified value and returns the position of where it was found
