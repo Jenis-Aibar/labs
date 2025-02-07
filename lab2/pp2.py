16b6400090dec030a9e64e5729b5f6cfe53cbc06
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:20:37 +0500

    Tuples

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,18 +1,83 @@
-'''
-Python - List Methods
-List Methods
-Python has a set of built-in methods that you can use on lists.
+# Python Tuples
+mytuple = ("apple", "banana", "cherry")
 
-Method	Description
-append()	Adds an element at the end of the list
-clear()	Removes all the elements from the list
-copy()	Returns a copy of the list
-count()	Returns the number of elements with the specified value
-extend()	Add the elements of a list (or any iterable), to the end of the current list
-index()	Returns the index of the first element with the specified value
-insert()	Adds an element at the specified position
-pop()	Removes the element at the specified position
-remove()	Removes the item with the specified value
-reverse()	Reverses the order of the list
-sort()	Sorts the list
-'''
+# Tuples are used to store multiple items in a single variable.
+
+# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+# A tuple is a collection which is ordered and unchangeable.
+
+# Tuples are written with round brackets.
+
+# Create a Tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# Tuple Items
+# Tuple items are ordered, unchangeable, and allow duplicate values.
+
+# Tuple items are indexed, the first item has index [0], the second item has index [1] etc.
+
+# Ordered
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+
+# Unchangeable
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+
+# Allow Duplicates
+# Since tuples are indexed, they can have items with the same value:
+
+# Tuples allow duplicate values:
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+# Tuple Length
+# To determine how many items a tuple has, use the len() function:
+# Print the number of items in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+# Create Tuple With One Item
+# To create a tuple with only one item, you have to add a comma after the item, otherwise Python will not recognize it as a tuple.
+# One item tuple, remember the comma:
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+# Tuple Items - Data Types
+# Tuple items can be of any data type:
+# String, int and boolean data types:
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+# A tuple can contain different data types:
+# A tuple with strings, integers and boolean values:
+
+tuple1 = ("abc", 34, True, 40, "male")
+type()
+# From Python's perspective, tuples are defined as objects with the data type 'tuple':
+
+<class 'tuple'>
+# What is the data type of a tuple?
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+# The tuple() Constructor
+# It is also possible to use the tuple() constructor to make a tuple.
+
+# Using the tuple() method to make a tuple:
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
