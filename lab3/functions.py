ad43d7b71dba43b91b274f096f41fba663260617
Aibar <a_jenissov@kbtu.kz>
2025-02-07 23:01:53 +0500

    functions

--- lab3/functions.py
+++ lab3/functions.py
@@ -1,96 +1,155 @@
-class MyClass:
-  x = 5
-
-
-
-p1 = MyClass()
-print(p1.x)
+def my_function():
+  print("Hello from a function")
 
 
 
 
+def my_function():
+    print("Hello from a function")
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
+my_function()
 
 
 
 
+def my_function(fname):
+  print(fname + " Refsnes")
 
-
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-p1 = Person("John", 36)
-
-print(p1)
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
 
 
 
 
+def my_function(fname, lname):
+  print(fname + " " + lname)
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-
-  def __str__(self):
-    return f"{self.name}({self.age})"
-
-p1 = Person("John", 36)
-
-print(p1)
+my_function("Emil", "Refsnes")
 
 
 
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+def my_function(fname, lname):
+  print(fname + " " + lname)
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
-
-p1 = Person("John", 36)
-p1.myfunc()
+my_function("Emil")
 
 
 
 
-class Person:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
 
-  def myfunc(abc):
-    print("Hello my name is " + abc.name)
+my_function("Emil", "Tobias", "Linus")
 
-p1 = Person("John", 36)
-p1.myfunc()
+
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
 
 
 
 
 
-p1.age = 40
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
 
-
-del p1.age
-
-
-del p1
+my_function(fname = "Tobias", lname = "Refsnes")
 
 
 
-class Person:
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+
+def myfunction():
   pass
+
+
+
+def my_function(x, /):
+  print(x)
+
+my_function(3)
+
+
+
+def my_function(x):
+  print(x)
+
+my_function(x = 3)
+
+
+def my_function(x, /):
+  print(x)
+
+my_function(x = 3)
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(x = 3)
+
+
+
+def my_function(x):
+  print(x)
+
+my_function(3)
+
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(3)
+
+
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
+
+
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
