f4e90837c2feb858312cde82f9a9fcca4792bebf
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:07:51 +0500

    Access_list_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,86 +1,64 @@
-# Python Lists
+# Python - Access List Items
 
-mylist = ["apple", "banana", "cherry"]
 
-# Lists are used to store multiple items in a single variable.
-
-# Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
-
-# Lists are created using square brackets:
-
-# Create a List:
+# Access Items
+# List items are indexed and you can access them by referring to the index number:
+# Print the second item of the list:
 
 thislist = ["apple", "banana", "cherry"]
-print(thislist)
+print(thislist[1])
 
-# List Items
-# List items are ordered, changeable, and allow duplicate values.
+#--------------------------------
+# Note: The first item has index 0.
+#--------------------------------
 
-# List items are indexed, the first item has index [0], the second item has index [1] etc.
+# Negative Indexing
+# Negative indexing means start from the end
 
-# Ordered
-# When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+# -1 refers to the last item, -2 refers to the second last item etc.
 
-# If you add new items to a list, the new items will be placed at the end of the list.
-
-#----------------------------------------------------------------------------------------------------------------------
-# Note: There are some list methods that will change the order, but in general: the order of the items will not change.
-#----------------------------------------------------------------------------------------------------------------------
-
-# Changeable
-# The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
-# Allow Duplicates
-# Since lists are indexed, lists can have items with the same value:
-
-# Lists allow duplicate values:
-
-thislist = ["apple", "banana", "cherry", "apple", "cherry"]
-print(thislist)
-
-# List Length
-# To determine how many items a list has, use the len() function:
-
-# Print the number of items in the list:
+# Print the last item of the list:
 
 thislist = ["apple", "banana", "cherry"]
-print(len(thislist))
+print(thislist[-1])
 
-# List Items - Data Types
-# List items can be of any data type:
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+# When specifying a range, the return value will be a new list with the specified items.
+# Return the third, fourth, and fifth item:
 
-# String, int and boolean data types:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+#------------------------------------------------------------------------------------
+# Note: The search will start at index 2 (included) and end at index 5 (not included).
+#------------------------------------------------------------------------------------
 
-list1 = ["apple", "banana", "cherry"]
-list2 = [1, 5, 7, 9, 3]
-list3 = [True, False, False]
+# Remember that the first item has index 0.
+# By leaving out the start value, the range will start at the first item:
+# This example returns the items from the beginning to, but NOT including, "kiwi":
 
-# A list can contain different data types:
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
 
-# A list with strings, integers and boolean values:
+# By leaving out the end value, the range will go on to the end of the list:
 
-list1 = ["abc", 34, True, 40, "male"]
-type()
+# This example returns the items from "cherry" to the end:
 
-# From Python's perspective, lists are defined as objects with the data type 'list':
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
 
-<class 'list'>
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the list:
 
-# What is the data type of a list?
+# This example returns the items from "orange" (-4) to, but NOT including "mango" (-1):
 
-mylist = ["apple", "banana", "cherry"]
-print(type(mylist))
-# The list() Constructor
-# It is also possible to use the list() constructor when creating a new list.
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+# Check if Item Exists
+# To determine if a specified item is present in a list use the in keyword:
 
-# Using the list() constructor to make a List:
+# Check if "apple" is present in the list:
 
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
-print(thislist)
-
-# Python Collections (Arrays)
-# There are four collection data types in the Python programming language:
-
-# List is a collection which is ordered and changeable. Allows duplicate members.
-# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
-# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
-# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+thislist = ["apple", "banana", "cherry"]
+if "apple" in thislist:
+  print("Yes, 'apple' is in the fruits list")
