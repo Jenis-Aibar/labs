9c8178cddf5a0726fb9811a711b97835de8c987f
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:40:39 +0500

    Dictionaries

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,27 +1,58 @@
-# Python - Set Methods
-# Set Methods
-# Python has a set of built-in methods that you can use on sets.
+# Python Dictionaries
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+# Dictionary
+# Dictionaries are used to store data values in key:value pairs.
 
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
 
-'''
-Method	Shortcut	Description
-add()	 	Adds an element to the set
-clear()	 	Removes all the elements from the set
-copy()	 	Returns a copy of the set
-difference()	-	Returns a set containing the difference between two or more sets
-difference_update()	-=	Removes the items in this set that are also included in another, specified set
-discard()	 	Remove the specified item
-intersection()	&	Returns a set, that is the intersection of two other sets
-intersection_update()	&=	Removes the items in this set that are not present in other, specified set(s)
-isdisjoint()	 	Returns whether two sets have a intersection or not
-issubset()	<=	Returns whether another set contains this set or not
- 	<	Returns whether all items in this set is present in other, specified set(s)
-issuperset()	>=	Returns whether this set contains another set or not
- 	>	Returns whether all items in other, specified set(s) is present in this set
-pop()	 	Removes an element from the set
-remove()	 	Removes the specified element
-symmetric_difference()	^	Returns a set with the symmetric differences of two sets
-symmetric_difference_update()	^=	Inserts the symmetric differences from this set and another
-union()	|	Return a set containing the union of sets
-update()	|=	Update the set with the union of this set and others
-'''
+# As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+# Dictionaries are written with curly brackets, and have keys and values:
+
+# Create and print a dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+# Dictionary Items
+# Dictionary items are ordered, changeable, and do not allow duplicates.
+
+# Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+
+# Print the "brand" value of the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+# Ordered or Unordered?
+# As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+# When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+
+# Unordered means that the items do not have a defined order, you cannot refer to an item by using an index.
+
+# Changeable
+# Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+
+# Duplicates Not Allowed
+# Dictionaries cannot have two items with the same key:
+
+# Duplicate values will overwrite existing values:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
