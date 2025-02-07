9a138f5eb91056c91bd62f5ce713046005638176
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:25:26 +0500

    Join_Tuples

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,35 +1,19 @@
-# Python - Loop Tuples
-# Loop Through a Tuple
-# You can loop through the tuple items by using a for loop.
+# Python - Join Tuples
+# Join Two Tuples
+# To join two or more tuples you can use the + operator:
+# Join two tuples:
 
-# Iterate through the items and print the values:
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
 
-thistuple = ("apple", "banana", "cherry")
-for x in thistuple:
-  print(x)
-# Learn more about for loops in our Python For Loops Chapter.
+tuple3 = tuple1 + tuple2
+print(tuple3)
+# Multiply Tuples
+# If you want to multiply the content of a tuple a given number of times, you can use the * operator:
 
-# Loop Through the Index Numbers
-# You can also loop through the tuple items by referring to their index number.
+# Multiply the fruits tuple by 2:
 
-# Use the range() and len() functions to create a suitable iterable.
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
 
-# Print all items by referring to their index number:
-
-thistuple = ("apple", "banana", "cherry")
-for i in range(len(thistuple)):
-  print(thistuple[i])
-# Using a While Loop
-# You can loop through the tuple items by using a while loop.
-
-# Use the len() function to determine the length of the tuple, then start at 0 and loop your way through the tuple items by referring to their indexes.
-
-# Remember to increase the index by 1 after each iteration.
-
-# Print all items, using a while loop to go through all the index numbers:
-
-thistuple = ("apple", "banana", "cherry")
-i = 0
-while i < len(thistuple):
-  print(thistuple[i])
-  i = i + 1
+print(mytuple)
