42f63b315e752d5b73e972176fa7a60c131a4b1a
Aibar <a_jenissov@kbtu.kz>
2025-02-07 05:15:24 +0500

    Assign_Multiple_Variables

--- pp.py
+++ pp.py
@@ -1,44 +1,29 @@
-# Python - Variable Names
+# Python Variables - Assign Multiple Values
 
-# Variable Names
-# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
-# A variable name must start with a letter or the underscore character
-# A variable name cannot start with a number
-# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
-# Variable names are case-sensitive (age, Age and AGE are three different variables)
-# A variable name cannot be any of the Python keywords.
+# Many Values to Multiple Variables
+# Python allows you to assign values to multiple variables in one line:
 
-#Legal variable names:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
 
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
+# One Value to Multiple Variables
+# And you can assign the same value to multiple variables in one line:
 
-#Illegal variable names:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
 
-2myvar = "John"
-my-var = "John"
-my var = "John"
+# Unpack a Collection
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
 
-#Multi Words Variable Names
-#Variable names with more than one word can be difficult to read.
+# Unpack a list:
 
-#There are several techniques you can use to make them more readable:
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
 
-#Camel Case
-#Each word, except the first, starts with a capital letter:
-
-myVariableName = "John"
-
-#Pascal Case
-#Each word starts with a capital letter:
-
-MyVariableName = "John"
-
-#Snake Case
-#Each word is separated by an underscore character:
-
-my_variable_name = "John"
