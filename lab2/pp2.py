77ac3eff0da748198f6b749b5f72d92b5ee32ca6
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:09:37 +0500

    Change_List_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,64 +1,46 @@
-# Python - Access List Items
+# Python - Change List Items
 
 
-# Access Items
-# List items are indexed and you can access them by referring to the index number:
-# Print the second item of the list:
-
-thislist = ["apple", "banana", "cherry"]
-print(thislist[1])
-
-#--------------------------------
-# Note: The first item has index 0.
-#--------------------------------
-
-# Negative Indexing
-# Negative indexing means start from the end
-
-# -1 refers to the last item, -2 refers to the second last item etc.
-
-# Print the last item of the list:
+# Change Item Value
+# To change the value of a specific item, refer to the index number:
+# Change the second item:
 
 thislist = ["apple", "banana", "cherry"]
-print(thislist[-1])
+thislist[1] = "blackcurrant"
+print(thislist)
 
-# Range of Indexes
-# You can specify a range of indexes by specifying where to start and where to end the range.
-# When specifying a range, the return value will be a new list with the specified items.
-# Return the third, fourth, and fifth item:
+# Change a Range of Item Values
+# To change the value of items within a specific range, define a list with the new values, and refer to the range of index numbers where you want to insert the new values:
+# Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
 
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:5])
-#------------------------------------------------------------------------------------
-# Note: The search will start at index 2 (included) and end at index 5 (not included).
-#------------------------------------------------------------------------------------
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
 
-# Remember that the first item has index 0.
-# By leaving out the start value, the range will start at the first item:
-# This example returns the items from the beginning to, but NOT including, "kiwi":
+# If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
 
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[:4])
-
-# By leaving out the end value, the range will go on to the end of the list:
-
-# This example returns the items from "cherry" to the end:
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[2:])
-
-# Range of Negative Indexes
-# Specify negative indexes if you want to start the search from the end of the list:
-
-# This example returns the items from "orange" (-4) to, but NOT including "mango" (-1):
-
-thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-print(thislist[-4:-1])
-# Check if Item Exists
-# To determine if a specified item is present in a list use the in keyword:
-
-# Check if "apple" is present in the list:
+# Change the second value by replacing it with two new values:
 
 thislist = ["apple", "banana", "cherry"]
-if "apple" in thislist:
-  print("Yes, 'apple' is in the fruits list")
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+#-----------------------------------------------------------------------------------------------------------------------
+# Note: The length of the list will change when the number of items inserted does not match the number of items replaced.
+#-----------------------------------------------------------------------------------------------------------------------
+
+# If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+# Change the second and third value by replacing it with one value:
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+# Insert Items
+# To insert a new list item, without replacing any of the existing values, we can use the insert() method.
+# The insert() method inserts an item at the specified index:
+
+# Insert "watermelon" as the third item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
