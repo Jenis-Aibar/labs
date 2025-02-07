f32a46fbb8f4940e04cadbffe4cbe3aeb24bf237
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:30:39 +0500

    Set_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,92 +1,22 @@
-# Python Sets
-myset = {"apple", "banana", "cherry"}
-# Set
-# Sets are used to store multiple items in a single variable.
+# Python - Access Set Items
+# Access Items
+# You cannot access items in a set by referring to an index or a key.
 
-# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+# But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the in keyword.
 
-# A set is a collection which is unordered, unchangeable*, and unindexed.
-
-# * Note: Set items are unchangeable, but you can remove items and add new items.
-
-# Sets are written with curly brackets.
-
-# ExampleGet your own Python Server
-# Create a Set:
-
-thisset = {"apple", "banana", "cherry"}
-print(thisset)
-# Note: Sets are unordered, so you cannot be sure in which order the items will appear.
-
-# Set Items
-# Set items are unordered, unchangeable, and do not allow duplicate values.
-
-# Unordered
-# Unordered means that the items in a set do not have a defined order.
-
-# Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
-
-# Unchangeable
-# Set items are unchangeable, meaning that we cannot change the items after the set has been created.
-
-# Once a set is created, you cannot change its items, but you can remove items and add new items.
-
-# Duplicates Not Allowed
-# Sets cannot have two items with the same value.
-
-# Duplicate values will be ignored:
-
-thisset = {"apple", "banana", "cherry", "apple"}
-
-print(thisset)
-# Note: The values True and 1 are considered the same value in sets, and are treated as duplicates:
-
-True and 1 is considered the same value:
-
-thisset = {"apple", "banana", "cherry", True, 1, 2}
-
-print(thisset)
-# Note: The values False and 0 are considered the same value in sets, and are treated as duplicates:
-
-False and 0 is considered the same value:
-
-thisset = {"apple", "banana", "cherry", False, True, 0}
-
-print(thisset)
-
-# Get the Length of a Set
-# To determine how many items a set has, use the len() function.
-
-# Get the number of items in a set:
+# Loop through the set, and print the values:
 
 thisset = {"apple", "banana", "cherry"}
 
-print(len(thisset))
-# Set Items - Data Types
-# Set items can be of any data type:
+for x in thisset:
+  print(x)
+# Check if "banana" is present in the set:
 
-# String, int and boolean data types:
+thisset = {"apple", "banana", "cherry"}
 
-set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
-set3 = {True, False, False}
-# A set can contain different data types:
+print("banana" in thisset)
+# Check if "banana" is NOT present in the set:
 
-# A set with strings, integers and boolean values:
+thisset = {"apple", "banana", "cherry"}
 
-set1 = {"abc", 34, True, 40, "male"}
-type()
-# From Python's perspective, sets are defined as objects with the data type 'set':
-
-<class 'set'>
-# What is the data type of a set?
-
-myset = {"apple", "banana", "cherry"}
-print(type(myset))
-# The set() Constructor
-# It is also possible to use the set() constructor to make a set.
-
-# Using the set() constructor to make a set:
-
-thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
-print(thisset)
+print("banana" not in thisset)
