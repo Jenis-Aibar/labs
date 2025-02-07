32999f48ff462a63546fe0bc1fc59e0cb4cf97ee
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:44:36 +0500

    Loop_Dictionaries

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,48 +1,26 @@
-# Python - Remove Dictionary Items
-# Removing Items
-# There are several methods to remove items from a dictionary:
-# The pop() method removes the item with the specified key name:
+# Python - Loop Dictionaries
+# Loop Through a Dictionary
+# You can loop through a dictionary by using a for loop.
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.pop("model")
-print(thisdict)
-# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
+# When looping through a dictionary, the return value are the keys of the dictionary, but there are methods to return the values as well.
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.popitem()
-print(thisdict)
-# The del keyword removes the item with the specified key name:
+# Print all key names in the dictionary, one by one:
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-del thisdict["model"]
-print(thisdict)
-# The del keyword can also delete the dictionary completely:
+for x in thisdict:
+  print(x)
+# Print all values in the dictionary, one by one:
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-del thisdict
-print(thisdict) #this will cause an error because "thisdict" no longer exists.
-# The clear() method empties the dictionary:
+for x in thisdict:
+  print(thisdict[x])
+# You can also use the values() method to return values of a dictionary:
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-thisdict.clear()
-print(thisdict)
+for x in thisdict.values():
+  print(x)
+# You can use the keys() method to return the keys of a dictionary:
+
+for x in thisdict.keys():
+  print(x)
+# Loop through both keys and values, by using the items() method:
+
+for x, y in thisdict.items():
+  print(x, y)
