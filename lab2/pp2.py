fc52abe059753aff8b0a55e072ec28e681a8d7e9
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:43:46 +0500

    Remove_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,24 +1,48 @@
-# Python - Add Dictionary Items
-# Adding Items
-# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+# Python - Remove Dictionary Items
+# Removing Items
+# There are several methods to remove items from a dictionary:
+# The pop() method removes the item with the specified key name:
 
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-thisdict["color"] = "red"
+thisdict.pop("model")
 print(thisdict)
-# Update Dictionary
-# The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
-
-# The argument must be a dictionary, or an iterable object with key:value pairs.
-
-# Add a color item to the dictionary by using the update() method:
+# The popitem() method removes the last inserted item (in versions before 3.7, a random item is removed instead):
 
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
-thisdict.update({"color": "red"})
+thisdict.popitem()
+print(thisdict)
+# The del keyword removes the item with the specified key name:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+# The del keyword can also delete the dictionary completely:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+print(thisdict) #this will cause an error because "thisdict" no longer exists.
+# The clear() method empties the dictionary:
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
