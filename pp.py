1922caeea7fb94dc83db59c7d5d9202322a8beed
Aibar <a_jenissov@kbtu.kz>
2025-02-07 05:32:10 +0500

    Strings

--- pp.py
+++ pp.py
@@ -1,31 +1,109 @@
-# Python Casting
+# Python Strings
 
-# Specify a Variable Type
-# There may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+# Strings
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
 
-# Casting in python is therefore done using constructor functions:
+# 'hello' is the same as "hello".
 
-int() - # constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
-float() - # constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
-str() - # constructs a string from a wide variety of data types, including strings, integer literals and float literals
+# You can display a string literal with the print() function:
 
-Integers:
+print("Hello")
+print('Hello')
 
-x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+# Quotes Inside Quotes
+# You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
 
-Floats:
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
 
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
+# Assign String to a Variable
+# Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
 
-Strings:
-
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+a = "Hello"
+print(a)
 
 
+# Multiline Strings
+# You can assign a multiline string to a variable by using three quotes:
+
+# You can use three double quotes:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# Or three single quotes:
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+# --------------------------------------------------------------------------------------
+# Note: in the result, the line breaks are inserted at the same position as in the code.
+# --------------------------------------------------------------------------------------
+
+# Strings are Arrays
+# Like many other popular programming languages, strings in Python are arrays of bytes representing unicode characters.
+
+# However, Python does not have a character data type, a single character is simply a string with a length of 1.
+
+# Square brackets can be used to access elements of the string.
+
+# Get the character at position 1 (remember that the first character has the position 0):
+
+a = "Hello, World!"
+print(a[1])
+
+
+# Looping Through a String
+# Since strings are arrays, we can loop through the characters in a string, with a for loop.
+
+# Loop through the letters in the word "banana":
+
+for x in "banana":
+  print(x)
+
+# String Length
+# To get the length of a string, use the len() function.
+
+# The len() function returns the length of a string:
+
+a = "Hello, World!"
+print(len(a))
+
+# Check String
+# To check if a certain phrase or character is present in a string, we can use the keyword in.
+
+# Check if "free" is present in the following text:
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+# Use it in an if statement:
+
+# Print only if "free" is present:
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+# Check if NOT
+# To check if a certain phrase or character is NOT present in a string, we can use the keyword not in.
+
+# Check if "expensive" is NOT present in the following text:
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+# Use it in an if statement:
+
+# print only if "expensive" is NOT present:
+
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
