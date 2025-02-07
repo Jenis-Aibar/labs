f3ce6915d317f4d15cf4f71ecc7633067b35b928
Aibar <a_jenissov@kbtu.kz>
2025-02-07 05:36:03 +0500

    Concatenate

--- pp.py
+++ pp.py
@@ -1,36 +1,18 @@
-# Python - Modify Strings
-# Python has a set of built-in methods that you can use on strings.
+# Python - String Concatenation
 
-# Upper Case
-# The upper() method returns the string in upper case:
+# String Concatenation
+# To concatenate, or combine, two strings you can use the + operator.
 
-a = "Hello, World!"
-print(a.upper())
+# Merge variable a with variable b into variable c:
 
-# Lower Case
-# The lower() method returns the string in lower case:
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
 
-a = "Hello, World!"
-print(a.lower())
+# To add a space between them, add a " ":
 
-# Remove Whitespace
-# Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
-
-# The strip() method removes any whitespace from the beginning or the end:
-
-a = " Hello, World! "
-print(a.strip()) # returns "Hello, World!"
-
-# Replace String
-# The replace() method replaces a string with another string:
-
-a = "Hello, World!"
-print(a.replace("H", "J"))
-
-# Split String
-# The split() method returns a list where the text between the specified separator becomes the list items.
-# The split() method splits the string into substrings if it finds instances of the separator:
-
-a = "Hello, World!"
-print(a.split(",")) # returns ['Hello', ' World!']
-
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
