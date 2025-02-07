245f5060d625d432398fc19a9c2f610e492192bd
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:37:28 +0500

    Remove_Set_Items

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,35 +1,49 @@
-# Python - Add Set Items
-# Add Items
-# Once a set is created, you cannot change its items, but you can add new items.
-
-# To add one item to a set use the add() method.
-
-# Add an item to a set, using the add() method:
+# Python - Remove Set Items
+# Remove Item
+# To remove an item in a set, use the remove(), or the discard() method.
+# Remove "banana" by using the remove() method:
 
 thisset = {"apple", "banana", "cherry"}
 
-thisset.add("orange")
+thisset.remove("banana")
 
 print(thisset)
-# Add Sets
-# To add items from another set into the current set, use the update() method.
+# Note: If the item to remove does not exist, remove() will raise an error.
 
-# Add elements from tropical into thisset:
+# Remove "banana" by using the discard() method:
 
 thisset = {"apple", "banana", "cherry"}
-tropical = {"pineapple", "mango", "papaya"}
 
-thisset.update(tropical)
+thisset.discard("banana")
 
 print(thisset)
-# Add Any Iterable
-# The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+# Note: If the item to remove does not exist, discard() will NOT raise an error.
 
-# Add elements of a list to at set:
+# You can also use the pop() method to remove an item, but this method will remove a random item, so you cannot be sure what item that gets removed.
+
+# The return value of the pop() method is the removed item.
+
+# Remove a random item by using the pop() method:
 
 thisset = {"apple", "banana", "cherry"}
-mylist = ["kiwi", "orange"]
 
-thisset.update(mylist)
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+# Note: Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
+# The clear() method empties the set:
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+# The del keyword will delete the set completely:
+
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
 
 print(thisset)
