ff9a731c78cb98563f0aad3cdbb020b7a72b348b
Aibar <a_jenissov@kbtu.kz>
2025-02-07 06:46:07 +0500

    Dictionarie_Methods

--- lab2/pp2.py
+++ lab2/pp2.py
@@ -1,57 +1,17 @@
-# Python - Nested Dictionaries
-# Nested Dictionaries
-# A dictionary can contain dictionaries, this is called nested dictionaries.
-
-# Create a dictionary that contain three dictionaries:
-
-myfamily = {
-  "child1" : {
-    "name" : "Emil",
-    "year" : 2004
-  },
-  "child2" : {
-    "name" : "Tobias",
-    "year" : 2007
-  },
-  "child3" : {
-    "name" : "Linus",
-    "year" : 2011
-  }
-}
-# Or, if you want to add three dictionaries into a new dictionary:
-
-# Create three dictionaries, then create one dictionary that will contain the other three dictionaries:
-
-child1 = {
-  "name" : "Emil",
-  "year" : 2004
-}
-child2 = {
-  "name" : "Tobias",
-  "year" : 2007
-}
-child3 = {
-  "name" : "Linus",
-  "year" : 2011
-}
-
-myfamily = {
-  "child1" : child1,
-  "child2" : child2,
-  "child3" : child3
-}
-# Access Items in Nested Dictionaries
-# To access items from a nested dictionary, you use the name of the dictionaries, starting with the outer dictionary:
-
-# Print the name of child 2:
-
-print(myfamily["child2"]["name"])
-# Loop Through Nested Dictionaries
-# You can loop through a dictionary by using the items() method like this:
-# Loop through the keys and values of all nested dictionaries:
-
-for x, obj in myfamily.items():
-  print(x)
-
-  for y in obj:
-    print(y + ':', obj[y])
+# Python Dictionary Methods
+# Dictionary Methods
+# Python has a set of built-in methods that you can use on dictionaries.
+'''
+Method	Description
+clear()	Removes all the elements from the dictionary
+copy()	Returns a copy of the dictionary
+fromkeys()	Returns a dictionary with the specified keys and value
+get()	Returns the value of the specified key
+items()	Returns a list containing a tuple for each key value pair
+keys()	Returns a list containing the dictionary's keys
+pop()	Removes the element with the specified key
+popitem()	Removes the last inserted key-value pair
+setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+update()	Updates the dictionary with the specified key-value pairs
+values()	Returns a list of all the values in the dictionary
+'''
