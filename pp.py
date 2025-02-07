8ad0d6e6bd870f9357456b732daa5be601bb2711
Aibar <a_jenissov@kbtu.kz>
2025-02-07 05:23:55 +0500

    Data_types

--- pp.py
+++ pp.py
@@ -1,59 +1,63 @@
-# Python - Global Variables
+# Python Data Types
 
-# Global Variables
-#Variables that are created outside of a function (as in all of the examples in the previous pages) are known as global variables.
+# Built-in Data Types
+# In programming, data type is an important concept.
 
-#Global variables can be used by everyone, both inside of functions and outside.
+# Variables can store data of different types, and different types can do different things.
 
-# Create a variable outside of a function, and use it inside the function
+# Python has the following data types built-in by default, in these categories:
 
-x = "awesome"
+# Text Type:	    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+# None Type:	    NoneType
 
-def myfunc():
-  print("Python is " + x)
+# Getting the Data Type
+# You can get the data type of any object by using the type() function:
 
-myfunc()
+# Print the data type of the variable x:
 
-#If you create a variable with the same name inside a function, this variable will be local, and can only be used inside the function. The global variable with the same name will remain as it was, global and with the original value.
+x = 5
+print(type(x))
 
-#Create a variable inside a function, with the same name as the global variable
 
-x = "awesome"
+#     Example	                           Data Type
+x = "Hello World"	                          #str	
+x = 20	                                      #int	
+x = 20.5	                                  #float	
+x = 1j	                                      #complex	
+x = ["apple", "banana", "cherry"]	          #list	
+x = ("apple", "banana", "cherry")	          #tuple	
+x = range(6)	                              #range	
+x = {"name" : "John", "age" : 36}	          #dict	
+x = {"apple", "banana", "cherry"}	          #set	
+x = frozenset({"apple", "banana", "cherry"})  #frozenset	
+x = True	                                  #bool	
+x = b"Hello"	                              #bytes	
+x = bytearray(5)	                          #bytearray	
+x = memoryview(bytes(5))	                  #memoryview	
+x = None                                      #NoneType
 
-def myfunc():
-  x = "fantastic"
-  print("Python is " + x)
 
-myfunc()
+# Setting the Specific Data Type
+# If you want to specify the data type, you can use the following constructor functions:
 
-print("Python is " + x)
-
-# The global Keyword
-# Normally, when you create a variable inside a function, that variable is local, and can only be used inside that function.
-
-# To create a global variable inside a function, you can use the global keyword.
-
-# If you use the global keyword, the variable belongs to the global scope:
-
-def myfunc():
-  global x
-  x = "fantastic"
-
-myfunc()
-
-print("Python is " + x)
-
-# Also, use the global keyword if you want to change a global variable inside a function.
-
-# To change the value of a global variable inside a function, refer to the variable by using the global keyword:
-
-x = "awesome"
-
-def myfunc():
-  global x
-  x = "fantastic"
-
-myfunc()
-
-print("Python is " + x)
-
+#     Example	                            Data Type
+x = str("Hello World")	                        #str	
+x = int(20)	                                    #int	
+x = float(20.5)	                                #float	
+x = complex(1j)	                                #complex	
+x = list(("apple", "banana", "cherry"))	        #list	
+x = tuple(("apple", "banana", "cherry"))	    #tuple	
+x = range(6)	                                #range	
+x = dict(name="John", age=36)	                #dict	
+x = set(("apple", "banana", "cherry"))	        #set	
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+x = bool(5)	                                    #bool	
+x = bytes(5)	                                #bytes	
+x = bytearray(5)	                            #bytearray	
+x = memoryview(bytes(5))	                    #memoryview
